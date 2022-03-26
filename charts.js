9247abab502d4c5fb8d5d06e30090017abcc9449
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-03-26 15:41:03 -0700

    updates

--- charts.js
+++ charts.js
@@ -64,28 +64,30 @@ function buildCharts(sample) {
     //  5. Create a variable that holds the first sample in the array.
     var result = filterArray[0];
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var ids = 
-
-    var labels =
-
-    var values =
-
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 
-
-    // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     
-    };
-    // 10. Use Plotly to plot the data with the layout. 
+    console.log(result)
     
-  });
+  //   // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+  //   var ids = 
+
+  //   var labels =
+
+  //   var values =
+
+  //   // 7. Create the yticks for the bar chart.
+  //   // Hint: Get the the top 10 otu_ids and map them in descending order  
+  //   //  so the otu_ids with the most bacteria are last. 
+
+  //   var yticks = 
+
+  //   // 8. Create the trace for the bar chart. 
+  //   var barData = [
+      
+  //   ];
+  //   // 9. Create the layout for the bar chart. 
+  //   var barLayout = {
+     
+  //   };
+  //   // 10. Use Plotly to plot the data with the layout. 
+    
+  // });
 }
