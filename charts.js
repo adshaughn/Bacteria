db3115f8aeff57b593d3ada28cb0f95d9a43987a
adshaughn <96558619+adshaughn@users.noreply.github.com>
2022-04-08 14:09:58 -0700

    Update charts.js

--- charts.js
+++ charts.js
@@ -109,3 +109,63 @@ function buildCharts(sample) {
 
   });
 }
+
+// Bubble Chart
+
+// Bar and Bubble charts
+// Create the buildCharts function.
+function buildCharts(sample) {
+  // Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+
+    // Create a variable that holds the samples array. 
+    var samples = data.samples;
+
+    var metadata = data.metadata
+    // Create a variable that filters the samples for the object with the desired sample number.
+    var filterArray = samples.filter(sampleObj => sampleObj.id == sample);
+
+    var metadataArray = metadata.filter(sampleObj => sampleObj.id == sample);
+
+    // Create a variable that holds the first sample in the array.
+    var result = filterArray[0];
+
+    var metadataResult = metadataArray[0];
+
+    console.log(result)
+
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var ids = result.otu_ids;
+
+    var labels = result.otu_labels;
+
+    var values = result.sample_values;
+    
+
+    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot(); 
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      x: ids,
+      y: values,
+      text: labels,
+      type: "bubble",
+      mode: "markers",
+      marker: 
+    ];
+    var data = [bubbleData];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "",
+      xaxis: {title:"OTU ID"},
+      yaxis: {title: ""}
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot(); 
+  });
+}
